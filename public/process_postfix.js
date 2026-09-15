0c3da8c045ce4435b1ae3d246e4109c764a1d83f
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-09-15 18:25:57 +0200

    refactor worker scripts

--- public/process_postfix.js
+++ public/process_postfix.js
@@ -1,16 +1,12 @@
     if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
-        const exit = (code) => {
-            self.postMessage({ exit: code });
-            self.close();
-        };
         const args = {{ARGS}};
-        main(...args).then(exit).catch(async error => {
-            await __modules__.stdout.writeln(error.message);
-            exit(100);
-        });
+        const code = await main(...args);
+        self.postMessage({ exit: code });
+        self.close();
     }
 } catch (error) {
-    await __modules__.stdout.writeln(error.message);
-    self.postMessage({ exit: 200 });
+    await __modules__.stderr.writeln(error.message);
+    await __modules__.stderr.writeln(error.stack);
+    self.postMessage({ exit: 100 });
     self.close();
 }
